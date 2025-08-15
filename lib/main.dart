60ec705d15db4af891c239b4a136851be08f65af
satyam <mishrasatyam9890@gmail.com>
2025-08-15 20:37:36 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:fresh_market_app/pages/splash_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
+  }
+}
