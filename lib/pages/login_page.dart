16f684d823d7e0c0fa5e4d75cb702e2abb3f16ab
satyam <mishrasatyam9890@gmail.com>
2025-08-17 18:00:45 +0530

    Login UI

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,9 +12,115 @@ class LoginPage extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Image.asset('assets/images/splash.png', height: 100),
+              Image.asset('assets/images/splash.png', height: 250),
               SizedBox(height: 20),
-              Text("LOGINPAGE", style: TextStyle(fontSize: 24)),
+              Column(
+                children: [
+                  Text(
+                    'Sign In',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Name',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 60,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(133, 200, 105, 71),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 10,
+                                  ),
+                                  border: InputBorder.none,
+                                  hintText: "Enter Username",
+                                  hintStyle: TextStyle(
+                                    color: Color.fromARGB(192, 255, 255, 255),
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Password',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  height: 60,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    color: Color.fromARGB(133, 200, 105, 71),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      contentPadding: EdgeInsets.symmetric(
+                                        horizontal: 10,
+                                      ),
+                                      border: InputBorder.none,
+                                      hintText: "Enter Password",
+                                      hintStyle: TextStyle(
+                                        color: Color.fromARGB(
+                                          192,
+                                          255,
+                                          255,
+                                          255,
+                                        ),
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 25),
+                                Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  padding: EdgeInsets.all(10),
+                                  child: FilledButton(
+                                    style: ButtonStyle(
+                                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                                    ),
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Sign In',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
